76e1d912659cc43f1b7a43ecc875275c3103dd29
arsen2019 <arsen.grigoryan555@gmail.com>
2024-06-11 23:33:32 +0400

    Add schedules route param

--- src/routes/rootRoute/routes/schedulesRoute/schedulesRoute.tsx
+++ src/routes/rootRoute/routes/schedulesRoute/schedulesRoute.tsx
@@ -3,12 +3,18 @@ import {rootRoute} from "../../rootRoute";
 
 export const schedulesRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: '/schedules',
-  component: function Index() {
+  path: '/schedules/$scheduleUuid',
+  component: Schedules
+})
+
+function Schedules() {
+    // TODO: find out why does not work without @ts-ignore
+    // @ts-ignore
+    const params = schedulesRoute.useParams();
+    console.log(params)
     return (
       <div className="p-2">
         <h3>Welcome schedules!</h3>
       </div>
     )
-  },
-})
+  }
