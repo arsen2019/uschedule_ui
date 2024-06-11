8f94355654d4b9fc7be40805170f5e72ff161c9c
arsen2019 <arsen.grigoryan555@gmail.com>
2024-06-11 23:51:59 +0400

    Install TanStackQuery

--- src/routes/rootRoute/routes/indexRoute/indexRoute.tsx
+++ src/routes/rootRoute/routes/indexRoute/indexRoute.tsx
@@ -1,14 +1,35 @@
 import {createRoute} from "@tanstack/react-router";
 import {rootRoute} from "../../rootRoute";
+import {QueryKey, useQuery} from "@tanstack/react-query";
+import {useState} from "react";
 
 export const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/',
-  component: function Index() {
+  component: Index,
+})
+
+function Index() {
+    const {data: groups, isLoading} = useQuery({
+        queryKey: ['groups'],
+        queryFn: () => {
+            return fetch('http://127.0.0.1:8000/groups')
+                .then((res) => {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                    return [];
+                })
+
+        }
+    });
+
+    console.log(groups);
+
     return (
       <div className="p-2">
         <h3>Welcome Home!</h3>
+          {isLoading && 'loading...'}
       </div>
     )
-  },
-})
+  }
