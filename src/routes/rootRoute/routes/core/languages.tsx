84ed419e4fc5b00140aa0fdf765cc101b55dd58c
arsen2019 <arsen.grigoryan555@gmail.com>
2024-09-25 01:39:40 +0400

    Added localization

---
+++ src/routes/rootRoute/routes/core/languages.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Avatar, Select} from 'antd';
+import {TLanguage} from "./pageContent";
+
+
+const {Option} = Select;
+
+interface LanguageSwitcherProps {
+    selectedLanguage: TLanguage;
+    setLanguage: (language: TLanguage) => void;
+}
+
+export const LanguageSwitcher: React.FC<LanguageSwitcherProps> = ({selectedLanguage, setLanguage}) => {
+    const handleChange = (value: TLanguage) => {
+        setLanguage(value);
+    };
+
+    return (
+        <Select defaultValue={selectedLanguage} onChange={handleChange}>
+            <Option value="hy"><img src="/icons/armenia.png" alt="Armenian"
+                                    style={{'width': '30px', 'display': 'flex', 'alignItems': 'center'}}/></Option>
+            <Option value="ru"><img src="/icons/russia.png" alt="Armenian"
+                                    style={{'width': '30px', 'display': 'flex', 'alignItems': 'center'}}/></Option>
+            <Option value="en"><img src="/icons/united-kingdom.png" alt="Armenian"
+                                    style={{'width': '30px', 'display': 'flex', 'alignItems': 'center'}}/></Option>
+        </Select>
+    );
+};
---
+++ src/routes/rootRoute/routes/core/pageContent.ts
@@ -0,0 +1,73 @@
+export type TLanguage = 'en' | 'ru' | 'hy'
+
+export const pageContent: Record<string, Record<TLanguage, string>> ={
+    'Teacher': {
+        'en':'Teacher',
+        'ru':'Преподаватель',
+        'hy':'Դասախոս',
+    },
+    'Room':{
+        'en':'Room',
+        'ru':'Аудитория',
+        'hy':'Լսարան',
+    },
+    'Building':{
+        'en':'Building',
+        'ru':'Корпус',
+        'hy':'Մասնաշենք',
+    },
+    'Monday':{
+        'en':'Monday',
+        'ru':'Понедельник',
+        'hy':'Երկուշաբթի',
+    },
+    'Tuesday':{
+        'en':'Tuesday',
+        'ru':'Вторник',
+        'hy':'Երեքշաբթի',
+    },
+    'Wednesday':{
+        'en':'Wednesday',
+        'ru':'Среда',
+        'hy':'Չորեքշաբթի',
+    },
+    'Thursday':{
+        'en':'Thursday',
+        'ru':'Четверг',
+        'hy':'Հինգշաբթի',
+    },
+    'Friday':{
+        'en':'Friday',
+        'ru':'Пятница',
+        'hy':'Ուրբաթ',
+    },
+    'Schedule':{
+        'en':'Schedule',
+        'ru':'Расписание',
+        'hy':'Դասացուցակ',
+    },
+    'Current Week':{
+        'en':'Current Week',
+        'ru':'Текущая неделя',
+        'hy':'Այս շաբաթ',
+    },
+    'Next Week':{
+        'en':'Next Week',
+        'ru':'Следущая неделя',
+        'hy':'Հաջորդ շաբաթ',
+    },
+    'Select a Group':{
+        'en':'Select a Group',
+        'ru':'Выберите Группу',
+        'hy':'Ընտրեք Խումբը',
+    },
+    'Group':{
+        'en':'Group',
+        'ru':'Группа',
+        'hy':'Խումբ',
+    }
+
+
+
+
+}
