72428915f1f9d786d470d6078470490a7b86069c
arsen2019 <arsen.grigoryan555@gmail.com>
2024-09-25 01:56:24 +0400

    Modified styles

--- src/routes/rootRoute/routes/core/groupSelect.tsx
+++ src/routes/rootRoute/routes/core/groupSelect.tsx
@@ -1,52 +1,42 @@
 import {useNavigate} from "@tanstack/react-router";
-import {useState} from "react";
 import {Group} from "../indexRoute/indexRoute";
 import {Select} from "antd";
-
-const selectStyle = {
-    display:'flex',
-    justifyContent:'space-evenly',
-    padding:'15px',
-}
+import {pageContent, TLanguage} from "./pageContent";
 
 type GroupSelectProps = {
     groups: Group[]
     isLoading: boolean
-    selectedGroup:Group | null
+    language : TLanguage
+    selectedGroup: Group | null
 }
 
-function GroupSelect({groups, isLoading, selectedGroup}: GroupSelectProps) {
+function GroupSelect({groups, isLoading, language, selectedGroup}: GroupSelectProps) {
     const navigate = useNavigate()
-    //const [selectedGroup, setSelectedGroup] = useState<Group | null>()
-
+    localStorage.setItem('groups', JSON.stringify(groups));
     const onGroupChange = (selectedGroupUuid: Group['uuid']) => {
 
         navigate({
             to: '/schedules/$scheduleUuid',
             params: {
                 scheduleUuid: selectedGroupUuid,
-            },
-            search: () => ({
-                groups: JSON.stringify(groups),
-                selectedGroup:JSON.stringify(groups?.find(group => group.uuid === selectedGroupUuid)),
-            }),
+            }
         });
     }
 
     return (
-        <div style={selectStyle}>
+        <div>
             <Select
-            loading={isLoading}
-            placeholder={selectedGroup ? selectedGroup.name : "Group"}
-            onChange={onGroupChange}
-            fieldNames={{
-                label: 'name',
-                value: 'uuid'
-            }}
-            style={{width: 220}}
-            options={groups}
-            value={selectedGroup ? selectedGroup.uuid : undefined}
-        />
+                loading={isLoading}
+                placeholder={selectedGroup ? selectedGroup.name : pageContent['Group'][language]}
+                onChange={onGroupChange}
+                fieldNames={{
+                    label: 'name',
+                    value: 'uuid'
+                }}
+                style={{width: 220}}
+                options={groups}
+                value={selectedGroup ? selectedGroup.uuid : undefined}
+            />
         </div>
 
     );
