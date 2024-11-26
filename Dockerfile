65771e3355692ad7705daa5d3fa4ed561202048a
arsen2019 <arsen.grigoryan555@gmail.com>
2024-11-27 00:14:15 +0400

    Generated Source Maps for Sentry and uploaded them

--- Dockerfile
+++ Dockerfile
@@ -3,14 +3,26 @@ FROM node:19.5.0-alpine AS builder
 WORKDIR /app
 
 COPY package*.json ./
+
 RUN npm install
+
+RUN npm install -g @sentry/cli
 
 COPY . .
 
+ARG SENTRY_AUTH_TOKEN
+ENV SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN
 ENV NODE_OPTIONS="--max_old_space_size=4096"
-
+ENV GENERATE_SOURCEMAP=true
 
 RUN npm run build
+
+RUN sentry-cli releases files 0.1.12 -p javascript-react --org university-y1 upload-sourcemaps /app/build  --validate
+
+#RUN sentry-cli releases files -p javascript-react --org university-y1 $(sentry-cli releases propose-version) upload-sourcemaps /app/build --url-prefix '~/static/js' --validate
+
+
+#RUN sentry-cli releases files "0.1.0" upload-sourcemaps /app/build --url-prefix '~/static/js' --validate
 
 
 FROM node:19.5.0-alpine
@@ -19,6 +31,8 @@ FROM node:19.5.0-alpine
 WORKDIR /app
 
 COPY --from=builder /app/build ./build
+
+COPY --from=builder /app/package.json ./package.json
 
 RUN npm install -g serve
 
--- src/index.tsx
+++ src/index.tsx
@@ -9,18 +9,18 @@ import * as Sentry from "@sentry/react";
 import ReactGA from "react-ga4";
 
 Sentry.init({
-  dsn: "https://fe8e094758ef6fca139ce96a2d404e57@o4508167457603584.ingest.de.sentry.io/4508167481786448",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  // Tracing
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/schedule\.arsgreg\.com\/api/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    dsn: "https://fe8e094758ef6fca139ce96a2d404e57@o4508167457603584.ingest.de.sentry.io/4508167481786448",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    release:'0.1.12',
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/schedule\.arsgreg\.com\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -30,7 +30,7 @@ const queryClient = new QueryClient()
 const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
 
 if (TRACKING_ID) {
-  ReactGA.initialize(TRACKING_ID);
+    ReactGA.initialize(TRACKING_ID);
 }
 
 root.render(
@@ -40,6 +40,3 @@ root.render(
         </QueryClientProvider>
     </React.StrictMode>
 );
-
-
-reportWebVitals(console.log);
